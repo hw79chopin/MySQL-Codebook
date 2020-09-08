fded06b668a747cab334e356af6ab357f46e352a
HyunWoo Jung <chopin_liszt@naver.com>
2020-09-08 14:07:35 +0900

    Upload first setting and 001, 002 query

---
+++ SQL 스터디/입문/001.sql
@@ -0,0 +1,2 @@
+/* 001. 테이블에서 특정 column 선택하기 */
+SELECT empno, ename, sal FROM emp;
---
+++ SQL 스터디/입문/002.sql
@@ -0,0 +1,4 @@
+/* 002. 테이블에서 모든 column 출력하기 */
+SELECT * FROM emp;
+
+SELECT emp.*, ename FROM emp;		/* 특정 칼럼을 마지막에 또 출력하는 방법 */
