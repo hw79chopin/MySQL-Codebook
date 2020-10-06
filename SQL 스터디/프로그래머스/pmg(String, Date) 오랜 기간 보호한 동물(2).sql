acfaae0379ad6a59846f2ab20a858c9d11744068
HyunWoo Jung <chopin_liszt@naver.com>
2020-10-06 14:24:37 +0900

    Code corrected

--- SQL 스터디/프로그래머스/pmg(String, Date) 오랜 기간 보호한 동물(2).sql
+++ SQL 스터디/프로그래머스/pmg(String, Date) 오랜 기간 보호한 동물(2).sql
@@ -1,9 +1,7 @@
--- Q. 
-SELECT TIMEDIFF(i.DATETIME,o.DATETIME)
-    FROM ANIMAL_INS i, ANIMAL_OUTS o;
-    
-    SELECT o.ANIMAL_ID, o.NAME
+-- Q. 입양을 간 동물 중, 보호 기간이 가장 길었던 동물 두 마리의 아이디와 이름을 조회하는 SQL문을 작성해주세요. 이때 결과는 보호 기간이 긴 순으로 조회해야 합니다.
+SELECT o.ANIMAL_ID, o.NAME
     FROM ANIMAL_OUTS o
     LEFT JOIN ANIMAL_INS i
         ON i.ANIMAL_ID = o.ANIMAL_ID
-    ORDER BY i.DATETIEM - o.DATETIME
+    ORDER BY o.DATETIME - i.DATETIME DESC
+    LIMIT 2;
